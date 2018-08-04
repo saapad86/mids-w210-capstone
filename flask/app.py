213d8e818cfe5c7f8c3c34b7b1d13e21ef1e01c7
Saad <spadela@jupyterhub.w210rms.com>
2018-08-04 06:21:48 +0000

    working navbar/detail page. needs styling

--- flask/app.py
+++ flask/app.py
@@ -15,6 +15,8 @@ from copy import deepcopy
 from bs4 import BeautifulSoup
 from flask import Flask,render_template,json,Response,jsonify,request,url_for
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
 
 import argparse
 
@@ -28,10 +30,28 @@ import urllib.parse
 app = Flask(__name__, static_folder='static')
 Bootstrap(app)
 
+nav = Nav()
+
+@nav.navigation()
+def mynavbar():
+    return Navbar(
+        'Decrypto',
+        View('Upcoming ICOs', 'upcoming'),
+        View('About', 'about')
+    )
+
+# ...
+
+nav.init_app(app)
+
 @app.route("/")
 @app.route("/upcoming")
 def upcoming():
 	return render_template('upcoming.html')
+
+@app.route("/upcoming2")
+def upcoming2():
+	return render_template('index.html')
 
 @app.route("/_upcoming1")
 def _upcoming1():
@@ -41,45 +61,57 @@ def _upcoming1():
 	df1.sort_values(by=['Symbol','Hype Score'],ascending=[False,False])
 	return jsonify(df1.fillna('').to_dict(orient='records'))
 
-@app.route("/summary")
-def summary():
-	return render_template('summary.html')
+@app.route("/about")
+def about():
+	return render_template('upcoming.html')
 
-@app.route("/_summary_data1/limit/<int:limit>")
-def summary_data1(limit=0):
-	# Select all records from icotracker table
-	result = db['icotracker_ico'].find({})
+# @app.route("/summary")
+# def summary():
+# 	return render_template('html/index.html')
 
-	data = []
-	for i,row in enumerate(result):
-		data.append(row)
-		# Add support for optional limit
+# @app.route("/_summary_data1/limit/<int:limit>")
+# def summary_data1(limit=0):
+# 	# Select all records from icotracker table
+# 	result = db['icotracker_ico'].find({})
 
-		if limit != 0 and i >= limit:
-			break
-	df1 = pd.DataFrame(data)[['company_url','description','ico_dates','ico_sold_status','name','owner_name','owner_linkedin_profile','tracker_url','whitepaper','status']]
-	df1['name_url'] = df1.apply(lambda x: str(x['name']) + '|' + '/detail/' + x['name'],axis=1)
-	df1['owner_url'] = df1.apply(lambda x: str(x['owner_name']) + '|' + str(x['owner_linkedin_profile']),axis=1)
-	def make_link(label,url):
-		return "<a href='{url}'>{label}</url>".format(label=label,url=url)
+# 	data = []
+# 	for i,row in enumerate(result):
+# 		data.append(row)
+# 		# Add support for optional limit
 
-	df1['links'] = df1.apply(lambda x: '<br/>'.join([make_link('URL',x['company_url']),make_link('Tracker',x['tracker_url']), \
-		make_link('Whitepaper',x['whitepaper']),make_link('Reddit',x['status'])]),axis=1)
+# 		if limit != 0 and i >= limit:
+# 			break
+# 	df1 = pd.DataFrame(data)[['company_url','description','ico_dates','ico_sold_status','name','owner_name','owner_linkedin_profile','tracker_url','whitepaper','status']]
+# 	df1['name_url'] = df1.apply(lambda x: str(x['name']) + '|' + '/detail/' + x['name'],axis=1)
+# 	df1['owner_url'] = df1.apply(lambda x: str(x['owner_name']) + '|' + str(x['owner_linkedin_profile']),axis=1)
+# 	def make_link(label,url):
+# 		return "<a href='{url}'>{label}</url>".format(label=label,url=url)
 
-	df1['sparkline_example'] = df1.apply(lambda x: 'Jan-18:500, Feb-18:600, Mar-18:800',axis=1)
-	df1 = df1[['name_url','owner_url','links','description','ico_dates','ico_sold_status','sparkline_example']]
-	df1.columns = ['ICO Name','Founder','Links','Description','Key Dates','Status','Price History']
-	return jsonify(df1.fillna('').to_dict(orient='records'))
+# 	df1['links'] = df1.apply(lambda x: '<br/>'.join([make_link('URL',x['company_url']),make_link('Tracker',x['tracker_url']), \
+# 		make_link('Whitepaper',x['whitepaper']),make_link('Reddit',x['status'])]),axis=1)
+
+# 	df1['sparkline_example'] = df1.apply(lambda x: 'Jan-18:500, Feb-18:600, Mar-18:800',axis=1)
+# 	df1 = df1[['name_url','owner_url','links','description','ico_dates','ico_sold_status','sparkline_example']]
+# 	df1.columns = ['ICO Name','Founder','Links','Description','Key Dates','Status','Price History']
+# 	return jsonify(df1.fillna('').to_dict(orient='records'))
 
 @app.route("/detail/<string:name>")
-def detail_data1(name):
-	# select data about symbol
-	filters = {'name': name}
-	df1 = db.toDF('sp_price_history',filters)
+def detail(name):
+	# # select data about symbol
+	# filters = {'name': name}
+	# df1 = db.toDF('sp_price_history',filters)
 	# dump to json
 	# data1 = json.dumps(db.sql(sql,params).to_dict(orient='records'))
 	# render template
 	return render_template('detail.html',ico_name=name)
+
+@app.route("/_detail/<string:name>")
+def _detail1(name):
+	filters = {'name': name}
+	df1 = db.toDF('sp_upcoming_summary_v2',filters)
+	df1 = df1[['symbol','name','hype_score','risk_score','sentiment_score','volatility_score','owner_url','links','description','ico_start_date','ico_end_date','ico_sold_status','bonus_details','twitter_followers','facebook_followers']]
+	# df1.columns = ['Symbol','ICO Name','Hype Score','Risk Score','Sentiment Score','Volatilty Score,'Founder','Links','Description','Start Date','End Date','Status','Price History']
+	return jsonify(df1.fillna('').to_dict(orient='records')[0])
 
 @app.route("/similar/<string:name>")
 def similar(name):
